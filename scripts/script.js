c635b51a47c6f3f2fc2b9788c169e9c6a7eb7cca
Lucas Flores <lucasnicolasflores@gmail.com>
2025-02-03 08:45:54 -0300

    agregada imagen del silicon en las tarjetas + comentarios en el js

--- scripts/script.js
+++ scripts/script.js
@@ -12,8 +12,8 @@ let cardTemplate = '<div class="card"><div class="back"></div><div class="face">
 
 function activate(e) {
    if (currentMove < 2) {
-      
-      if ((!selectedCards[0] || selectedCards[0] !== e.target) && !e.target.classList.contains('active') ) {
+
+      if ((!selectedCards[0] || selectedCards[0] !== e.target) && !e.target.classList.contains('active')) {
          e.target.classList.add('active');
          selectedCards.push(e.target);
 
@@ -58,17 +58,21 @@ function getFaceValue(value) {
    return rtn;
 }
 
-for (let i=0; i < totalCards; i++) {
+for (let i = 0; i < totalCards; i++) {
    let div = document.createElement('div');
    div.innerHTML = cardTemplate;
    cards.push(div);
-   document.querySelector('#game').append(cards[i]);
+   document.querySelector('#game').append(cards[i]); // Append FIRST
+
    randomValue();
    cards[i].querySelectorAll('.face')[0].innerHTML = getFaceValue(valuesUsed[i]);
-   cards[i].querySelectorAll('.card')[0].addEventListener('click', activate);
+
+   // Add event listener AFTER appending:
+   cards[i].querySelector('.card').addEventListener('click', activate); // More specific selector
+
 }
 
 resetButton.addEventListener('click', () => {
-  window.location.reload();
+   window.location.reload();
 });
 
